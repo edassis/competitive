88daf373734459c15d84fc37cc7c8f4f7f7b723a
Eduardo Assis <eduardoffassis@gmail.com>
2022-03-07 20:00:52 -0300

    Segment, Line structs

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ using vpl = vector<pl>;
 #define forn(i, n)     for (int i = 0; i < (int)n; i++)
 #define forni(i, k, n) for (int i = k; i < (int)n; i++)
 #define forne(i, k, n) for (int i = k; i <= (int)n; i++)
-#define fora(a)        for (auto &each : a)
+#define fora(a)        for (auto each : a)
 #define all(x)         x.begin(), x.end()
 #define rall(x)        x.rbegin(), x.rend()
 #define clr(x)         memset(x, 0, sizeof(x))
@@ -44,17 +44,13 @@ inline void sws() {
 }
 
 // Constants
-const double pi  = acos(-1.0);
-const ll     MOD = 1e9 + 7;
+const ll     MOD = 998244353;
 const ll     oo  = 1e18 + 7;
-
+const double PI  = acos(-1.0);
 const double EPS{1e-6};
 
 template <typename T>
 struct Point;
-
-template <typename T>
-struct Line;
 
 template <typename T>
 bool equals(const T &a, const T &b) {
@@ -67,16 +63,7 @@ bool equals(const T &a, const T &b) {
 
 template <typename T>
 double dist(const Point<T> &a, const Point<T> &b) {
-    return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
-}
-
-template <typename T>
-T manhattan(const Point<T> &a, const Point<T> &b) {
-    if (is_floating_point_v<T>) {
-        return fabs(a.x - b.x) + fabs(a.y - b.y);
-    } else {
-        return abs(a.x - b.x) + abs(a.y - b.y);
-    }
+    return hypot(static_cast<double>(a.x - b.x), static_cast<double>(a.y - b.y));
 }
 
 template <typename T>
@@ -90,7 +77,6 @@ struct Point {
 
 template <typename T>
 struct Line {
-    // equacação geral
     T a, b, c;
 
     Line(const Point<T> &P, const Point<T> &Q) {
@@ -98,73 +84,114 @@ struct Line {
         b = Q.x - P.x;
         c = P.x * Q.y - Q.x * P.y;
 
-        T div = equals(a, 0.0) ? b : a;
-        a /= div;
-        b /= div;
-        c /= div;
+        // T div = equals(a, 0.0) ? b : a;
+        // a /= div;
+        // b /= div;
+        // c /= div;
     }
 
-    bool operator==(const Line<T> &l) const {
-        return equals(a, l.a) && equals(b, l.b) && equals(c, l.c);
+    // bool operator==(const Line<T> &l) const {
+    //     return equals(a, l.a) && equals(b, l.b) && equals(c, l.c);
+    // }
+
+    double distance(const Point<T> &p) const {
+        return fabs(a * p.x + b * p.y + c) / hypot(a, b);
+    }
+
+    bool contains(const Point<T> &p) const {
+        return equals(a * p.x + b * p.y + c, 0);
+    }
+
+    Point<T> closest(const Point<T> &p) const {
+        auto den = (a * a + b * b);
+
+        auto x = (b * (b * p.x - a * p.y) - a * c) / den;
+        auto y = (a * (-b * p.x + a * p.y) - b * c) / den;
+
+        return Point<T>{x, y};
     }
 };
 
 template <typename T>
-struct Triangle {
-    Point<T> A, B, C;
+struct Segment {
+    Point<T> A, B;
 
-    double area() const {
-        // Fórmula de Heron
-        auto a = dist(A, B);
-        auto b = dist(B, C);
-        auto c = dist(C, A);
+    bool contains2(const Point<T> &P) const {
+        double dAB = dist(A, B), dAP = dist(A, P), dPB = dist(P, B);
 
-        // semiperímetro
-        auto s = (a + b + c) / 2;
+        return equals(dAP + dPB, dAB);
+    }
 
-        return sqrt(s) * sqrt(s - a) * sqrt(s - b) * sqrt(s - c);
+    Point<T> closest(const Point<T> &P) {
+        Line<T> r(A, B);
+        auto    Q = r.closest(P);
+
+        if (this->contains2(Q))
+            return Q;
+
+        auto distA = dist(P, A);
+        auto distB = dist(P, B);
+
+        if (distA <= distB)
+            return A;
+        else
+            return B;
     }
 };
 
 int main() {
     sws();
 
-    int t;
-    cin >> t;
+    int n, vb, vs;
+    int xu, yu;
+    cin >> n >> vb >> vs;
 
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
+    vi parada(n);
+    forn(i, n) {
+        cin >> parada[i];
+    }
 
-        vector<Point<int>> points;
-        vi                 has(n);
+    cin >> xu >> yu;
 
-        forn(i, n) {
-            Point<int> p;
-            cin >> p.x >> p.y;
-            points.pb(p);
-        }
+    Point<int> A{0, 0};
+    double     t   = 0;
+    int        idx = 0;
+    forni(i, 1, n) {
+        Point<int> U{xu, yu};
+        Point<int> B{parada[i], 0};
 
-        forn(i, n) {
-            forni(j, i + 1, n) {
-                auto man = manhattan(points[i], points[j]);
-                if (man <= k) {
-                    has[i]++;
-                }
-            }
-        }
+        auto t1 = dist(A, U) / vs;
 
-        bool valido = false;
-        forn(i, n) {
-            if (has[i] == n - 1) {
-                valido = true;
+        auto t2b = dist(A, B) / vb;
+        auto t2s = dist(B, U) / vs;
+
+        W(t1);
+        W(t2b);
+        W(t2s);
+
+        if (t + t1 < t + t2b + t2s) {
+            // ir a pe
+            // idx = i - 1;
+            t += t1;
+            break;
+        } else if (t + t1 == t + t2b + t2s) {
+            auto d1 = dist(A, U);
+            auto d2 = dist(B, U);
+            if (d1 < d2) {
+                // desce se parada leva o mesmo tempo e a pé é for mais perto
+                // idx = i - 1;
+                t += t1;
                 break;
             }
         }
 
-        cout << (valido ? "1" : "-1") << endl;
+        // caso contrario, continuar no onibus
+        idx = i;
+        t += t2b;
+        A = B;
     }
+
+    cout << idx + 1 << endl;
 
     return 0;
 }
-
