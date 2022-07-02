1c3221f73bb0a44689f613bcbe23942cbc11f15a
Eduardo Assis <eduardoffassis@gmail.com>
2022-07-02 01:18:26 -0300

    Fix bits include :D

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 ::-D CMAKE_C_COMPILER=C:/dev/TDM-GCC-32/bin/mingw32-gcc.exe -D CMAKE_CXX_COMPILER=C:/dev/TDM-GCC-32/bin/mingw32-g++.exe
-cmake -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER=c:/Users/eduar/scoop/apps/gcc/current/bin/gcc.exe -D CMAKE_CXX_COMPILER=c:/Users/eduar/scoop/apps/gcc/current/bin/g++.exe -Hd:/Repositorios/competitive -Bd:/Repositorios/competitive/build -G "Unix Makefiles"
+@REM cmake -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER=c:/Users/eduar/scoop/apps/gcc/current/bin/gcc.exe -D CMAKE_CXX_COMPILER=c:/Users/eduar/scoop/apps/gcc/current/bin/g++.exe -Hd:/Repositorios/competitive -Bd:/Repositorios/competitive/build -G "Unix Makefiles"
+cmake -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER=c:/Users/eduar/scoop/apps/gcc/current/bin/gcc.exe -D CMAKE_CXX_COMPILER=c:/Users/eduar/scoop/apps/gcc/current/bin/g++.exe -B build -G "Unix Makefiles"
