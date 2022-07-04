5ca15cec54f50f5fd08fa4c106433c7c13762349
Eduardo Assis <eduardoffassis@gmail.com>
2022-07-03 23:17:03 -0300

    Fix gdb windows path
    Trying https://codeforces.com/group/jVE1fvdVf6/contest/387066/problem/B

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,96 @@
 #include <bits/stdc++.h>
 
 #ifndef ONLINE_JUDGE
-#define W(x) cerr << "\033[31m" << #x << " = " << x << "\033[0m" \
-                  << "\n";
+    #define W(x) cerr << "\033[31m" << #x << " = " << x << "\033[0m" << "\n";
 #else
-#define W(x)
+    #define W(x)
 #endif
 
 using namespace std;
 
 // Types
-using ll  = long long;
-using pii = pair<int, int>;
-using pl  = pair<ll, ll>;
-using vi  = vector<int>;
-using vl  = vector<ll>;
+using ll = long long;
+using pii = pair<int,int>;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
 using vpi = vector<pii>;
 using vpl = vector<pl>;
 
 // Macros
-#define pb             push_back
-#define eb             emplace_back
-#define mp             make_pair
-#define mt             make_tuple
-#define ff             first
-#define ss             second
-#define endl           '\n'
-#define forn(i, n)     for (int i = 0; i < (int)n; i++)
-#define forni(i, k, n) for (int i = k; i < (int)n; i++)
-#define forne(i, k, n) for (int i = k; i <= (int)n; i++)
-#define fora(a)        for (auto each : a)
-#define all(x)         x.begin(), x.end()
-#define rall(x)        x.rbegin(), x.rend()
-#define clr(x)         memset(x, 0, sizeof(x))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define ff first
+#define ss second
+#define endl '\n'
+#define forn(i, n) for(int i=0; i<(int)n; i++)
+#define forni(i, k, n) for(int i=k; i<(int)n; i++)
+#define forne(i, k, n) for(int i=k; i<=(int)n; i++)
+#define fora(e, a) for(auto e:a)
+#define dbg(msg, var) cout << msg << " " << var << endl
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
 
 // Functions
-// template<typename T>
-// inline void clr(T& x) { memset(x, 0, sizeof(x)); }
-inline void sws() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-}
+template<typename T>
+inline void clr(T& x) { memset(x, 0, sizeof(x)); }
+inline void sws() { ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); }
 
 // Constants
-const ll     MOD = 1e9 + 7;
-const ll     oo  = 1e18 + 7;
-const double PI  = acos(-1.0);
-const double EPS{1e-6};
+const ll MOD = 1e9+7;
+const ll oo = 1e18+7;
+
+const int N = 3e5+1;
+vl w(N);
+vpi adj[N];
+int n=0;
+ll ans=0;
+
+ll f(int e, int p=0) {
+
+    ll best = w[e];
+
+    vl results;
+
+    for(auto [c, v] : adj[e]) {
+        if(v != p) {
+            auto child = f(v, e)-c;
+            results.pb(child);
+            best = max(best, child+w[e]);
+        }
+    }
+
+    sort(rall(results));
+
+    ans = max(ans, best);
+    if(results.size() >= 2) {
+        ans = max(ans, results[0]+results[1]+w[e]);
+    }
+
+    return best;
+}
 
 int main() {
     sws();
-    string a;
-    cin >> a;
-    int k;
-    cin >> k;
 
-    forn(i, a.size()) if(k) {
-        string end;
-        auto offset = 0;
-        forne(j, 1, k) if (i+j < a.size()) {
-            if (a[i+j] > a[i+offset]) { offset = j; }
-        }
+    cin >> n;
 
-        end = a.substr(0, i) + a[i+offset] + a.substr(i, offset);
-
-        if (i+offset+1 < a.size()) {
-            end += a.substr(i+offset+1, a.size() - (i+offset+1));
-        }
-        a=end;
-        k-=offset;
+    forn(i,n) {
+        cin >> w[i];
     }
 
-    cout << a << endl;
+    forn(i, n-1) {
+        int u, v, c; cin >> u >> v >> c;
+        u--; v--;
+        adj[u].eb(c, v);
+        adj[v].eb(c, u);
+    }
+    
+    f(0);
+
+    cout << ans << endl;
 
     return 0;
 }
--- src/main2.cpp
+++ src/main2.cpp
@@ -1,100 +1,77 @@
 #include <bits/stdc++.h>
 
 #ifndef ONLINE_JUDGE
-#define W(x) cerr << "\033[31m" << #x << " = " << x << "\033[0m" \
-                  << "\n";
+    #define W(x) cerr << "\033[31m" << #x << " = " << x << "\033[0m" << "\n";
 #else
-#define W(x)
+    #define W(x)
 #endif
 
 using namespace std;
 
 // Types
-using ll  = long long;
-using pii = pair<int, int>;
-using pl  = pair<ll, ll>;
-using vi  = vector<int>;
-using vl  = vector<ll>;
+using ll = long long;
+using pii = pair<int,int>;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
 using vpi = vector<pii>;
 using vpl = vector<pl>;
 
 // Macros
-#define pb             push_back
-#define eb             emplace_back
-#define mp             make_pair
-#define mt             make_tuple
-#define ff             first
-#define ss             second
-#define endl           '\n'
-#define forn(i, n)     for (int i = 0; i < (int)n; i++)
-#define forni(i, k, n) for (int i = k; i < (int)n; i++)
-#define forne(i, k, n) for (int i = k; i <= (int)n; i++)
-#define fora(a)        for (auto each : a)
-#define all(x)         x.begin(), x.end()
-#define rall(x)        x.rbegin(), x.rend()
-#define clr(x)         memset(x, 0, sizeof(x))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define ff first
+#define ss second
+#define endl '\n'
+#define forn(i, n) for(int i=0; i<(int)n; i++)
+#define forni(i, k, n) for(int i=k; i<(int)n; i++)
+#define forne(i, k, n) for(int i=k; i<=(int)n; i++)
+#define fora(a) for(auto each:a)
+#define dbg(msg, var) cout << msg << " " << var << endl
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
 
 // Functions
-// template<typename T>
-// inline void clr(T& x) { memset(x, 0, sizeof(x)); }
-inline void sws() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-}
+template<typename T>
+inline void clr(T& x) { memset(x, 0, sizeof(x)); }
+inline void sws() { ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); }
 
 // Constants
-const ll MOD = 1e9 + 7;
-const ll oo  = 1e18 + 7;
-const double PI  = acos(-1.0);
-const double EPS{1e-6}; 
-
-using inter = pair<int,int>;
-
-int solve() {
-    return 0;
-}
-
-ll f(ll n) {
-    return n * (n-1) * (n-2) / 6;
-}
+const ll MOD = 1e9+7;
+const ll oo = 1e18+7;
 
 int main() {
     sws();
-    
-    while(true) {
-        int n; cin >> n;
-        if(!n) break;    
 
-        int idx = 0;
-        double bigger = 0;
+    ll n; cin >> n;
 
-        forn(i, n) {
-            double w, h; cin >> w >> h;
-            
-            // 2x2
-            auto lmin = w/2;
-            lmin = min(lmin, h/2);
-            // W(lmin);
-            if(lmin - EPS > bigger) {
-                bigger = lmin;
-                idx = i+1;
-            }
+    set<ll> pcs;
 
-            // sequencia na linha/coluna
-            auto aux = max(w, h)/min(w, h);
-            // W(aux);
-            if(aux + EPS < 4) {
-            } else {
-                if(min(w, h) - EPS > bigger) {
-                    bigger = min(w,h);
-                    idx = i+1;
-                }
-            }
+    forn(i, n) {
+        ll x; cin >> x;
+        pcs.insert(x);
+    }
+
+    ll res = 0;
+    while(pcs.size() > 1) {
+        ll a = *pcs.begin();
+        ll b = *pcs.rbegin();
+
+        W(a);
+        W(b);
+        forn(j, 2<<(pcs.size()-2)) {
+            res = (res + abs(a-b)) % MOD;
         }
 
-        cout << idx << endl;
+        pcs.erase(pcs.begin());
+        
+        a = *(pcs.begin()+1);
+
     }
+
+    cout << res << endl;
 
     return 0;
 }
